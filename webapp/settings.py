495e4ef71eaf3a8fc59f3c4d04a1d665f72deaad
Yelson Chevarrias <chevarrias@gmail.com>
2018-08-27 00:29:29 -0500

    Fix settings for heroku

--- webapp/settings.py
+++ webapp/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
-    '',
     'apps.usuarios',
 ]
 
--- webapp/settings_heroku.py
+++ webapp/settings_heroku.py
@@ -10,131 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import django_heroku
-import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from webapp.settings import *
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%y-wjzik*y^ka&uj5pdl4o!7k9y$upj&n&*i27-5e(g0axze8g'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    '.localhost.pe',
-    'boiling-temple-49703.herokuapp.com'
-]
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-INSTALLED_APPS += [
-    'apps.usuarios'
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'webapp.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'webapp.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gjdctids',
-        'USER': 'gjdctids',
-        'PASSWORD': 'zWBbB0cHJIxiDA-pGTBK2F_TgI8-unOF',
-        'HOST': 'stampy.db.elephantsql.com',
-        'PORT': '',
-        # 'URL': 'postgres://gjdctids:zWBbB0cHJIxiDA-pGTBK2F_TgI8-unOF@stampy.db.elephantsql.com:5432/gjdctids',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
-
-LANGUAGE_CODE = 'es-pe'
-
-# TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_produccion')
-
-AUTH_USER_MODEL = 'usuarios.User'
 
 
 # Extra config
@@ -142,7 +22,5 @@ CSRF_TRUSTED_ORIGINS = [
     '.localhost.pe',
     'boiling-temple-49703.herokuapp.com'
 ]
-
-# SESSION_COOKIE_SAMESITE = None
 
 django_heroku.settings(locals())
