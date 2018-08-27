0ab7620dc2122ebe345fa306659839277e3a10ce
Yelson Chevarrias <chevarrias@gmail.com>
2018-08-27 00:24:01 -0500

    add Django Extensions APP

--- webapp/settings.py
+++ webapp/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 INSTALLED_APPS += [
-    'apps.usuarios'
+    '',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
