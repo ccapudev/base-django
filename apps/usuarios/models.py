cf42129b8345a6ec0022fc60fc6c24563ba87921
Yelson Chevarrias <chevarrias@gmail.com>
2018-08-26 23:15:27 -0500

    Version django 2.1

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -7,6 +7,10 @@ class User(AbstractUser):
     last_name_two = models.CharField(
         _('last name 2'), max_length=30, blank=True)
 
+class Publicacion(models.Model):
+	fecha = models.DateTimeField("fecha", auto_now_add=True)
+	fecha_modificacion = models.DateTimeField("fecha", auto_now=True)
+
 
 class Perfil(models.Model):
     pass
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from apps.usuarios.models import Publicacion
+
 
 # Create your views here.
+def home(request):
+	p, c = Publicacion.objects.get_or_create(pk=1)
+	return render(request, 'index.html', dict(
+		publi=p
+	))
