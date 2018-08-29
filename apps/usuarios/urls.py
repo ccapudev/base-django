6389047a33108519693783d9789ab58ba4c12a1c
Yelson Chevarrias <chevarrias@gmail.com>
2018-08-29 13:21:42 +0800

    Timezone support

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -4,6 +4,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('jwt/', obtain_jwt_token, name='token')
 ]
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from apps.usuarios.models import Publicacion
+from datetime import datetime
+from django.utils.timezone import utc
+from django.utils import timezone
 
 
 # Create your views here.
-def home(request):
-	p, c = Publicacion.objects.get_or_create(pk=1)
-	return render(request, 'index.html', dict(
-		publi=p
-	))
+class HomeView(APIView):
+	def get(self, request):
+		p, c = Publicacion.objects.get_or_create(pk=1)
+		if request.GET.get("json"):
+			return Response({
+				"fecha": p.fecha,
+				"fecha_modificacion": p.fecha_modificacion,
+				"ahora": datetime.utcnow().replace(tzinfo=utc),
+				"ahora_timezone": timezone.now(),
+			})
+
+		return render(request, 'index.html', dict(
+			publi=p
+		))
